7e5bd086b9833da282a02446e160c4cfdf4fd047
Sanjay Rana <Sanjayrana5113@gmail.com>
2025-05-13 10:20:06 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://isanjayrana2005:Bca2005@cluster0.avkyu86.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://isanjayrana2005:Bca2005@cluster0fooddel.8gztd6n.mongodb.net/food-del').then(()=>console.log("DB Connected"));
    
 }
 
