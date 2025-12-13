cdd64d63c9fec8c3ca27af60f413149f2dd2ef30
Sanjyrana <Sanjayrana5113@gmail.com>
2025-12-13 21:38:20 +0530

    image field updated

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -4,7 +4,16 @@ const foodSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true},
-    image: { type: String, required: true },
+    image: {
+         public_id:{
+            type:String,
+            required:true
+         },
+         url:{
+            type:String,
+            required:true
+         }
+        },
     category:{ type:String, required:true}
 })
 
